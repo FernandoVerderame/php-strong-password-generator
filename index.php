3f6cc739667a4fec13b5a22232d05e42457e2dce
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-02-21 18:31:52 +0100

    added validation form script

--- index.php
+++ index.php
@@ -2,25 +2,9 @@
 
 $page_title = 'Home';
 
-require __DIR__ . '/includes/utils/functions.php'; 
+require_once __DIR__ . '/includes/utils/functions.php'; 
 
-// Get sent data
-$password_length = $_GET['password-length'] ?? '';
-
-if($password_length) {
-    $is_password_valid = generate_password($password_length);
-
-    if ($is_password_valid) {
-
-        session_start();
-        $_SESSION['password'] = $is_password_valid;
-
-        header('Location: ./success.php');
-    }
-
-    $alert_class = 'alert-danger';
-    $alert_message = ' DEVI inserire la lunghezza della password!';
-}
+require __DIR__ . '/includes/scripts/form_validation.php';
 
 ?>
 
