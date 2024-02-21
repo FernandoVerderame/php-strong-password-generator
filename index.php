f5ec31f92c170bc132f4b997f05714a27df96f37
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-02-21 18:13:37 +0100

    added head into layouts

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
+$page_title = 'Home';
+
 require __DIR__ . '/includes/utils/functions.php'; 
 
 // Get sent data
@@ -25,13 +27,8 @@ if($password_length) {
 <!DOCTYPE html>
 <html lang="en" data-bs-theme="dark">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <title>Password Generator</title>
-</head>
+<!-- Head -->
+<?php include __DIR__ . '/includes/layouts/head.php' ?>
 
 <body class="bg-primary-subtle">
     <div class="container mt-3">
--- success.php
+++ success.php
@@ -4,19 +4,17 @@ session_start();
 
 $alert_class = 'alert-info';
 $alert_message = "La tua password è: <strong>{$_SESSION['password']}</strong>";
+$page_title = 'Success Page';
 
 ?>
 
 
 <!DOCTYPE html>
 <html lang="en" data-bs-theme="dark">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <title>Success</title>
-</head>
+
+<!-- Head --> 
+<?php include __DIR__ . '/includes/layouts/head.php' ?>
+
 <body class="bg-primary-subtle">
     <div class="container">
         <div class="password-alert mt-4">
